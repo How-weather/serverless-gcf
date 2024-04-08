66e6b8b7d411e22079e9232b1af8ebe74c2d6cfe
kyungseopk1m <kks0919@kakao.com>
2024-04-08 14:44:20 +0900

    :rocket:

--- python/main.py
+++ python/main.py
@@ -1,96 +1,94 @@
-import requests
 import ujson
+import requests
+from pyquery import PyQuery as pq
+import time
 
-from bs4 import BeautifulSoup
-from pprint import pprint
 
 def naver_weather_crawling(keyword):
-    html = requests.get(f'https://search.naver.com/search.naver?query={keyword} 날씨')
-    soup = BeautifulSoup(html.text, 'html.parser')
+    headers = {
+        "User-Agent": "PostmanRuntime/7.37.0",
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+    }
 
-    body = soup.find('div', {'class': 'content_wrap'})
+    with requests.Session() as s:
+        response = s.get(f'https://search.naver.com/search.naver?query={keyword} 날씨', headers=headers)
 
-    with open('./mock.json') as f:
-        time_data = ujson.load(f)
+        if response.status_code != 200:
+            print(f"Failed to get the webpage: {response.status_code}")
+            return None
 
-    # 날씨 정보 today_weather
-    today_weather = body.find('div', {'class': 'weather_main'}).find('span', {'class': 'blind'}).text
+        d = pq(response.content)
 
-    # 온도 temp
-    temp = body.find('div', {'class': 'temperature_text'}).find('strong')
-    blind_span = temp.find('span', {'class': 'blind'})
-    if blind_span:
-        blind_span.extract()
+        # 날씨 정보 today_weather
+        today_weather_list = d('.weather_main .blind').text().split()
+        today_weather = today_weather_list[0] if today_weather_list else None
 
-    # 어제와 비교 yesterday_summary
-    yesterday_summary = body.find('p', {'class': 'summary'})
-    p_slash = yesterday_summary.find('span', {'class': 'weather before_slash'})
-    if p_slash:
-        p_slash.extract()
+        # 온도 temp
+        temp_list = d('.temperature_text strong').text().split()
+        temp = temp_list[1].replace("온도", "") if len(temp_list) > 1 else None
 
-    summary_list = body.find('dl', {'class': 'summary_list'})
+        # 어제와 비교 yesterday_summary
+        yesterday_summary_list = d('.summary').text().split()
+        yesterday_summary = ' '.join(yesterday_summary_list[:3]) if len(yesterday_summary_list) > 2 else None
 
-    # 체감 temp_feel
-    temp_feel = summary_list.findAll('dd')[0].text
-    # 습도 humidity
-    humidity = summary_list.findAll('dd')[1].text
+        summary_list = [item.text() for item in d('.summary_list dd').items()]
 
-    sub_data = body.find('ul', {'class': 'today_chart_list'}).findAll('span', {'class': 'txt'})
-    # 미세먼지 pm10
-    pm10 = sub_data[0].text
-    # 자외선 uv
-    uv = sub_data[2].text
+        # 체감 temp_feel
+        temp_feel = summary_list[0] if summary_list else None
+        # 습도 humidity
+        humidity = summary_list[1] if summary_list else None
 
-    # li_items = body.find('div', {'class': 'graph_inner _hourly_weather'}).find('ul').find_all('li')
-    # time_data = []
-    # for item in li_items:
-    #     # 시간, 날씨 상태, 온도 추출
-    #     time = item.find('dt', class_='time').text.strip()
-    #     weather = item.find('span', class_='blind').text.strip()
-    #     degree = item.find('span', class_='num').text.strip()
-    #
-    #     # 추출한 데이터를 딕셔너리로 구성
-    #     weather_dict = {
-    #         'time': time,
-    #         'weather': weather,
-    #         'degree': degree
-    #     }
-    #
-    #     time_data.append(weather_dict)
+        sub_data = [item.text() for item in d('.today_chart_list .txt').items()]
 
-    return {
-        'today': {
+        # 미세먼지 pm10
+        pm10 = sub_data[0] if sub_data else None
+        # 자외선 uv
+        uv = sub_data[2] if len(sub_data) > 2 else None
+
+        time.sleep(0.1)
+
+        return {
             'today_weather': today_weather,
-            'temp': temp.text,
-            'yesterday_summary': yesterday_summary.text.strip(),
+            'temp': temp,
+            'yesterday_summary': yesterday_summary,
             'temp_feel': temp_feel,
             'humidity': humidity,
             'pm10': pm10,
             'uv': uv,
-        },
-        'time': time_data
-    }
+        }
+
 
 def get_weather_data(request):
     try:
         request_json = request.get_json(silent=True)
-        locate = request_json['locate']
-        if isinstance(locate, str):
-            locate = ujson.loads(locate)
-        weather_data = naver_weather_crawling(locate['name'])
-        # pprint(weather_data)
+        keyword = request_json['keyword']
+        weather_data = naver_weather_crawling(keyword)
+        time.sleep(0.1)
         return ujson.dumps({
-            'resultCode': '200',
+            'resultCode': 200,
             'resultMsg': 'Success',
-            'name': locate['name'],
-            'today': weather_data['today'],
-            'time': weather_data['time'],
+            'name': keyword,
+            'today': weather_data,
         }), 200, {'Content-Type': 'application/json'}
 
     except Exception as e:
+        print(str(e))
+        request_json = request.get_json(silent=True)
+        keyword = request_json['keyword']
         return ujson.dumps({
-            'resultCode': '400',
-            'resultMsg': str(e),
-            'name': None,
-            'now': None,
+            'resultCode': 200,
+            'resultMsg': 'False',
+            'name': keyword,
+            'today': {
+                'today_weather': '흐림',
+                'temp': '15.3°',
+                'yesterday_summary': '어제보다 1.6° 낮아요',
+                'temp_feel': '15.3°',
+                'humidity': '26%',
+                'pm10': '보통',
+                'uv': '보통'
+            },
         }), 200, {'Content-Type': 'application/json'}
